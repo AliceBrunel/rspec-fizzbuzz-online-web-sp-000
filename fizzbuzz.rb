d778c2d8c1b3f6063881f09e5d58723a99aee16f
Alice Brunel <alc.brunel@gmail.com>
2019-07-01 18:35:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -16,6 +16,14 @@ end
 def fizzbuzz(int)
    if (int % 3 == 0) || (int % 5 == 0)
     "Fizzbuzz"
+  end
+end
+
+def fizzbuzz(int)
+   if int % 3 == 0
+     "Fizz"
+   elsif int % 5 == 0
+     "Buzz"
    else
     "nil"
   end
